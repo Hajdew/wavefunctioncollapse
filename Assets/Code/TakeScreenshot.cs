8457e81b9666357c482a3c452a87f1233a91c35d
Hajdew <dglzab@gmail.com>
2021-09-03 19:43:05 +0200

    Updating to 2020.3.17f1 Unity version
    Updating to 2020.3.17f1 Unity version also disabling Screen Capture cos it caused problems

--- Assets/Code/TakeScreenshot.cs
+++ Assets/Code/TakeScreenshot.cs
@@ -38,7 +38,7 @@ public class TakeScreenshot : MonoBehaviour {
         }
 		var filename = this.getFilename();
 		System.IO.Directory.CreateDirectory(System.IO.Path.GetDirectoryName(filename));
-		ScreenCapture.CaptureScreenshot(filename);
+		// ScreenCapture.CaptureScreenshot(filename);  // DOESNT WORK IN 2020.3.17F1 Unity
         Debug.Log("Saved screenshot: " + filename);
         this.Counter++;
     }
